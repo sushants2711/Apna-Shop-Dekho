e48ea8764fe22a75ddbc2e459c1de49a2fe837ac
sushant kumar singh <sushants2711@gmail.com>
2025-08-19 20:59:36 +0530

    Update categoryDetailsData.js

--- frontend/src/API/ProductAPI/categoryDetailsData.js
+++ frontend/src/API/ProductAPI/categoryDetailsData.js
@@ -1,6 +1,6 @@
 export const categoryDetailsData = async (category) => {
     try {
-        const url = `http://localhost:5500/api/products/category/${category}`;
+        const url = `https://apna-shop-dekho-backend.onrender.com/api/products/category/${category}`;
         
         const response = await fetch(url, {
             method: "GET",
