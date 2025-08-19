b09a0e709eead54343e8e5471a1c20332692acb2
sushant kumar singh <sushants2711@gmail.com>
2025-08-19 21:00:03 +0530

    Update sort.home.js

--- frontend/src/API/ProductAPI/sort.home.js
+++ frontend/src/API/ProductAPI/sort.home.js
@@ -2,9 +2,9 @@ export const sortApi = async (value) => {
     let newUrl = "";
 
     if (value === "asc") {
-        newUrl = `http://localhost:5500/api/products?asc=${value}`;
+        newUrl = `https://apna-shop-dekho-backend.onrender.com/api/products?asc=${value}`;
     } else if (value === "dsc") {
-        newUrl = `http://localhost:5500/api/products?dsc=${value}`;
+        newUrl = `https://apna-shop-dekho-backend.onrender.com/api/products?dsc=${value}`;
     }
 
     const response = await fetch(newUrl, {
