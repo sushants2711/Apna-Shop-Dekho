71dc3dd5cae0974ba6af06b00c96048ddecdd842
sushant kumar singh <sushants2711@gmail.com>
2025-08-19 22:03:08 +0530

    Update send.cookies.js

--- backend/middleware/send.cookies.js
+++ backend/middleware/send.cookies.js
@@ -13,8 +13,8 @@ export const sendCookies = async (userId, res) => {
         res.cookie("jwt", token, {
             maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in millisecond
             httpOnly: true, // prevent from XSS attacks cross-site scripting attacks
-            sameSite: "Strict", // CSRF attacks cross-site request forgery attacks
-            secure: process.env.NODE_ENV !== "development",
+            sameSite: "None", // CSRF attacks cross-site request forgery attacks
+            secure: true
         });
     } catch (error) {
         return res.status(500).json({
