371314d9026a1812722cf7b3b4830fbec820d6a8
sushant kumar singh <sushants2711@gmail.com>
2025-08-19 20:58:30 +0530

    Update allProductDetails.js

--- frontend/src/API/ProductAPI/allProductDetails.js
+++ frontend/src/API/ProductAPI/allProductDetails.js
@@ -1,6 +1,6 @@
 export const allProductDetails = async (id) => {
     try {
-        const url = `http://localhost:5500/api/products/${id}`;
+        const url = `https://apna-shop-dekho-backend.onrender.com/api/products/${id}`;
         // console.log(url)
         const response = await fetch(url, {
             method: "GET",
